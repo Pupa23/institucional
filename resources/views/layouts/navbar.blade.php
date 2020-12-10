04cc9a107c035740c5c05efcacea7ca250063eb9
jesu suarez <jesu7gnr@gmail.com>
2020-12-10 12:57:45 -0300

    first commit

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm mb-3">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand ps-3" href="#">
+        <img src="{{ url('/images/logo.png') }}" alt="" width="30" height="24" class="d-inline-block align-top">
+        <span style="font-weight: bold" class="pl-2">Bootstrap</span>
+    </a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+            </li>
+        </ul>
+
+        <div class="position-absolute end-0">
+            <span class="text-primary fs-3 fw-bold">Call: </span><span class="fs-2" style="padding-right: 20px">2604848484</span>
+        </div>
+
+    </div>
+</nav>
