9e0073616ce3315a02c71ac0f8622228250ebada
jesus suarez <jesu7gnr@gmail.com>
2021-01-06 16:23:48 -0300

    Feature/add template (#1)
    * add template
    
    * template
    
    Co-authored-by: Guillermo Gurrieri <kcoonnelly@gmail.com>

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+
+@extends('layouts.layout')
+
+@section('main')
+    @include('components.carousel.index')
+    @include('components.about.index')
+    @include('components.whyUs.index')
+    @include('components.services.index')
+    @include('components.callToAction.index')
+    @include('components.portfolio.index')
+    @include('components.pricing.index')
+    @include('components.faq.index')
+    @include('components.contact.index')
+@endsection
