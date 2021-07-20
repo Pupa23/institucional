fc81b2053605062308c0d527e194c1433af2a570
facundozapata <facundo@creativedog.agency>
2021-07-20 20:28:49 -0300

    solucion confictos entorno deb

--- database/seeders/MainTableSeeder.php
+++ database/seeders/MainTableSeeder.php
@@ -17,8 +17,8 @@ class MainTableSeeder extends Seeder
         DB::table('mains')->insert([
             'title' => 'Title of section main',
             'subtitle' => 'Sub-title of section main lorem20',
-            'description_about' => 'Description del about Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi enim, eum expedita hic mollitia natus nemo nesciunt nihil perferendis, quaerat quas repellat velit veritatis voluptatem.',
-            'description_service' => 'Description of section Services Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet blanditiis commodi corporis deleniti dolorem, ex exercitationem molestiae nemo placeat, porro, reprehenderit voluptatum.',
+            'description_about' => 'Description del about Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi enim, eum expedita hic mollitia natus nemo ',
+            'description_service' => 'Description of section Services Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet blanditiis commodi corporis deleniti',
             'description_cta' => 'Description of Call To Action Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid facilis maiores temporibus!',
             'url_image' => 'carousel.jpg'
         ]);
